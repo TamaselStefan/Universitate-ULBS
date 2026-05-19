000b5cde3f6e9f4048fa1a601008a0736e3efffb
BalindruCW <59843767+TamaselStefan@users.noreply.github.com>
2026-05-19 11:47:25 +0300

    Revert "lab8., uitat sa trimit"
    This reverts commit 800254287732714d1f7855461d8e3f0a01a11eb3.

--- Java/Cantemir/Tema/src/Main.java
+++ Java/Cantemir/Tema/src/Main.java
@@ -19,9 +19,9 @@ public class Main {
         String cheieCautare = prenume + "-" + nume;
         Student gasit = gaseste.get(cheieCautare);
 
-//        if (gasit != null && gasit.getNota() != null) {
-//            return gasit.getNota();
-//        }
+        if (gasit != null && gasit.getNota() != null) {
+            return gasit.getNota(); // autounboxing OK
+        }
 
         return 0.0f;
     }
--- Java/Cantemir/Tema/src/student/Student.java
+++ Java/Cantemir/Tema/src/student/Student.java
@@ -4,7 +4,7 @@ public class Student {
 
     private final int numarMatricol;
     private final String prenume, nume, formatieDeStudiu;
-    private double nota=0;
+    private final Float nota = 1.0f;
 
     public Student() {
         numarMatricol = 0;
@@ -13,13 +13,6 @@ public class Student {
         formatieDeStudiu = "Nespecificat";
     }
 
-    public Student(int nr, String pre, String num, String form, double nota) {
-        numarMatricol = nr;
-        prenume = pre;
-        nume = num;
-        formatieDeStudiu = form;
-        this.nota=nota;
-    }
     public Student(int nr, String pre, String num, String form) {
         numarMatricol = nr;
         prenume = pre;
@@ -43,12 +36,10 @@ public class Student {
         return formatieDeStudiu;
     }
 
-    public double getNota() {
+    public Float getNota() {
         return nota;
     }
-    public void setNota(double nota) {
-        this.nota = nota;
-    }
+
 
     @Override
     public String toString() {
